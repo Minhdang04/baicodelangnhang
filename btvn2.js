94fe77af5a75e27e8aecd3e1b0d42ddfaf7d0392
Minhdang04 <80468760+Minhdang04@users.noreply.github.com>
2021-05-14 20:49:13 +0700

    Update btvn2.js

--- btvn2.js
+++ btvn2.js
@@ -6,22 +6,10 @@ function click() {
     let d = document.getElementById('gender').value;
     if (d == 'male'){
         let e = (10*Number(c)) + (25/4*Number(a)) - ((123/35)*Number(b)) - 5
-        alert(('Your BMR is ' + e))
+        document.getElementById('ketqua').innerHTML = 'Your BMR is ' + e;
     }
     else if (d == 'female'){
         let f = (10*Number(c)) + (25/4*Number(a)) - ((123/35)*Number(b)) - 161
-        alert('Your BMR is ' + f)
+        document.getElementById('ketqua').innerHTML = 'Your BMR is ' + f;
     }
-}function myFunction() {
-    // Get the checkbox
-    var checkBox = document.getElementById("myCheck");
-    // Get the output text
-    var text = document.getElementById("text");
-  
-    // If the checkbox is checked, display the output text
-    if (checkBox.checked == true){
-      text.style.display = "block";
-    } else {
-      text.style.display = "none";
-    }
-  }
+}
